1be7907055776d110b2b22e11bd41d6dfbb3aba4
ts367844 <tushar367844@gmail.com>
2026-01-03 21:41:56 +0530

    list

---
+++ 3_list.py
@@ -0,0 +1,17 @@
+# list
+# list to store your favourite destination in india
+destinations = ['Rajsthan','Ladakh','Andaman','Anini','Amarnath']
+# list to store 10 different type of values  
+information = ['bhavnagar','gujarat','india',364001,145.60,'AOT',True,44,None,'tushar']
+#  print all values 
+print(destinations,information)
+#  print 1st value 
+print(destinations[0],information[0])
+#  print 1st five value 
+print(information[0:5])
+# print last 5 value 
+print(information[4:9])
+#  print list twice using * 
+print(destinations*2)
+# print item in the list from 2nd to 5th position
+print(information[1:5])
