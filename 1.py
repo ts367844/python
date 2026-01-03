980fab7f16d488d142a09a45b50389b78722ace3
ts367844 <tushar367844@gmail.com>
2026-01-03 21:11:27 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,9 @@
+#write a program to create variable and display it
+name = 'tushar'
+address = 'ring road'
+city = 'bhavnagar'
+pincode = 364001
+age = 20
+weight = 69.70
+gender = 'male'
+print("name:",name,"address:",address,"city:",city,"pincode:",pincode,"age:",age,"weight:",weight,"gender:",gender)
---
+++ 2.py
@@ -0,0 +1,6 @@
+#user input
+college_name = input("Enter college name: ")
+branch = input("Enter branch: ")
+semester = input("Enter semester: ")
+hobbies = input("Enter hobbies (comma separated): ")
+print(college_name,branch,semester,hobbies)
