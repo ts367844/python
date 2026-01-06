8ef7a9b27f7f167b3bf13ebc6ac453aeec6d059f
ts367844 <tushar367844@gmail.com>
2026-01-06 11:42:43 +0530

    tuple

---
+++ 7_tuple_1.py
@@ -0,0 +1,14 @@
+# tuple 
+student = ("Tushar", 44, "Computer Science", True)
+product = (101, "Laptop", 55999.50, "In Stock")
+print(student)
+# disply 1 index
+print(student[0])
+# display 2 to last index
+print(product[1:])
+# find out the positon 
+print(student.index("Computer Science"))
+# print a two time tuple
+print(student*2)
+# merge a touple
+print(student+product)
