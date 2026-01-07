a5f2ce902f171c5f140e057b0680db147f1cfcab
ts367844 <tushar367844@gmail.com>
2026-01-07 12:05:36 +0530

    dictionary

---
+++ 9_dictionary_1.py
@@ -0,0 +1,13 @@
+# dictionary 1
+user_data = {'name':'tushar','age':20,'weight':73.00,'gender':True,'isMarried':False}
+print(user_data)
+print(user_data['name'])
+# update a value
+user_data['name'] = 'tushar makwana'
+# add a new values
+user_data['place'] = 'bhavnagar'
+user_data['pincode'] = 364001
+print(user_data)
+# delete a values
+del user_data['weight']
+print(user_data)
