c6fe52bae5edbdd326cdcde42d900fc3fa39d1d3
ts367844 <tushar367844@gmail.com>
2026-01-07 11:49:42 +0530

    tuple exercise

---
+++ 8_tuple_2.py
@@ -0,0 +1,11 @@
+# tuple 2
+indian_states = ("Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Goa","Gujarat","Haryana","Himachal Pradesh","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttar Pradesh","Uttarakhand","West Bengal")
+print(indian_states)
+# display 1st 5 item
+print(indian_states[0:5])
+# display 2,3,4  item
+print(indian_states[2:5])
+# display items from 7th position
+print(indian_states[7:])
+# remove 3 item
+# del indian_states[2]
