34d6caac5defa3025e1eae61efb3c85132c94c4c
ts367844 <tushar367844@gmail.com>
2026-01-07 22:56:53 +0530

    dictionary

---
+++ 10_dictionary_2.py
@@ -0,0 +1,18 @@
+# dictionary 2
+data = {}
+print(data)
+data['name'] = 'python data'
+data['time'] = 120
+data['topics'] = 'multiple'
+data['ispaid'] = False
+print(data)
+
+# add tuple and list into dictionary
+data['imp_chapter'] = (1,4,5,9,44,88)
+data['topic_name'] = ['loop','list','dictionary','tuple']
+print(data)
+# display 1 index
+print(data['name'])
+# display imp_chapter random data
+print(data['imp_chapter'][2])
+
