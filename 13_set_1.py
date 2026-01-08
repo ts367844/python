6bbc7faabff74865605250ce9f7f42b5af752646
ts367844 <tushar367844@gmail.com>
2026-01-08 22:20:45 +0530

    set

---
+++ 13_set_1.py
@@ -0,0 +1,6 @@
+# in set no having a specific index and sequence
+states = {"Gujarat","Mahrastra","Uttar Pradesh","Mizoram","Madhaya Pradesh","Jarkhand"}
+print(states)
+# remove a value from set
+states.remove("Uttar Pradesh")
+print(states)
