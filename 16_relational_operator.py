2513aae057080af37ae2421b52c9da6408190150
ts367844 <tushar367844@gmail.com>
2026-01-10 00:04:08 +0530

    relational_operator

---
+++ 16_relational_operator.py
@@ -0,0 +1,21 @@
+# relational operator in python
+n1 = int(input("Enter number 1:"))
+n2 = int(input("Enter number 2:"))
+
+res = n1 == n2 
+print(f"{res} = {n1} == {n2}") 
+
+res = n1 != n2 
+print(f"{res} = {n1} != {n2}") 
+
+res = n1 < n2 
+print(f"{res} = {n1} < {n2}") 
+
+res = n1 > n2 
+print(f"{res} = {n1} > {n2}") 
+
+res = n1 <= n2 
+print(f"{res} = {n1} <= {n2}") 
+
+res = n1 >= n2 
+print(f"{res} = {n1} >= {n2}") 
