6a365aee13299a1ee9ef5a2a6d5904090cfa1605
ts367844 <tushar367844@gmail.com>
2026-01-13 11:50:18 +0530

    list exercise

--- exercise/10_exe_list_count.py
+++ exercise/10_exe_list_count.py
@@ -1,5 +1,5 @@
 # Count how many times a specific value appears in a list using built-in methods
-n1 = (1,2,3,4,5,6,7,4,3)
+n1 = [1,2,3,4,5,6,7,4,3]
 
 # user input
 no = int(input("Enter the number : "))
---
+++ exercise/11_exe_list_unique.py
@@ -0,0 +1,6 @@
+# Convert a list with duplicate values into a set and display unique elements
+n1 = [1,2,3,4,5,6,7,4,3]
+
+# create a set from an list
+set1 = set(n1)
+print(set1)
