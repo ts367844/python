341cc9027adc332d59858e13025b3835c62d6763
ts367844 <tushar367844@gmail.com>
2026-01-13 12:19:38 +0530

    asc & desc of an list

---
+++ exercise/15_exe_list_asc_desc.py
@@ -0,0 +1,8 @@
+#  Given a list of numbers, sort it in ascending and descending order
+n1 = [1,10,3,6,87,4,44,76]
+# ascending order
+n1.sort()
+print(f"ascending order :{n1}")
+# descending order
+n1.reverse()
+print(f"descending order :{n1}")
