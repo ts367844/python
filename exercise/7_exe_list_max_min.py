7f0e04ed32f74dcdfd742ec37ec9469ec26e4acd
ts367844 <tushar367844@gmail.com>
2026-01-13 11:15:27 +0530

    max & min

---
+++ exercise/7_exe_list_max_min.py
@@ -0,0 +1,6 @@
+# Given a list of numbers, find the maximum and minimum values
+list1 = [1,2,3,4,5,6,7,8,9,10]
+
+# using built in function for max and min
+print("Maximum value is : ",max(list1))
+print("Minimum value is : ",min(list1))
