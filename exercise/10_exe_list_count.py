8b7daed0e2007e433828aad99f05919b0e11ed8a
ts367844 <tushar367844@gmail.com>
2026-01-13 11:43:43 +0530

    count a value of an list

---
+++ exercise/10_exe_list_count.py
@@ -0,0 +1,7 @@
+# Count how many times a specific value appears in a list using built-in methods
+n1 = (1,2,3,4,5,6,7,4,3)
+
+# user input
+no = int(input("Enter the number : "))
+
+print(f"{no} is in list {n1.count(no)} times")
