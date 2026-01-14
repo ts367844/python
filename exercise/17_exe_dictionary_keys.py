e6366d07a3091e4b01954965328bf2420e30480f
ts367844 <tushar367844@gmail.com>
2026-01-14 21:08:07 +0530

    dictionary

--- exercise/17_exe_dictionary_keys.py
+++ exercise/17_exe_dictionary_keys.py
@@ -1,4 +1,5 @@
 # From a dictionary, extract only the keys into a list
 student = {"a":75,"b":80,"c":66,"d":99,"e":59}
-# get a keys from an keys
-print(f"keys are : {student.keys()}")
+# getting a keys from an dictionary
+list1 = list(student.keys())
+print(f"keys are : {list1}")
---
+++ exercise/18_exe_dictionary_values.py
@@ -0,0 +1,5 @@
+# From a dictionary, extract only the values into a list
+student = {"a":75,"b":80,"c":66,"d":99,"e":59}
+# getting a values from an dictionary
+list1 = list(student.values())
+print(f"keys are : {list1}")
