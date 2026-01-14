1cd5db03e1f4ba18a1fe22f94ba69fadd961b958
ts367844 <tushar367844@gmail.com>
2026-01-14 20:58:50 +0530

    sum of an students mark

---
+++ exercise/16_exe_dictionary_tot.py
@@ -0,0 +1,6 @@
+# Create a dictionary of 5 students with their marks and find the total marks
+student = {"a":75,"b":80,"c":66,"d":99,"e":59}
+
+# sum of marks
+sum_mark = sum(student.values())
+print(f"Total marks : {sum_mark}")
