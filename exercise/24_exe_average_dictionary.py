2cb0ac86aa2dfd95712b62fa85b96f402a79751b
ts367844 <tushar367844@gmail.com>
2026-01-15 19:32:11 +0530

    average of an dictionary

---
+++ exercise/24_exe_average_dictionary.py
@@ -0,0 +1,4 @@
+# Create a dictionary and calculate the average of all values
+values1 = {"a":85,"b":80,"c":75,"d":90}
+salary = (values1.values())
+print(f"Average is : ",{sum(salary)/len(salary)})
