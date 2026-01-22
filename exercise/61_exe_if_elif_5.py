26189653fdd677b0cdb728a84172aa33b7a8ea1f
ts367844 <tushar367844@gmail.com>
2026-01-22 23:04:50 +0530

    if elif statement

--- exercise/61_exe_if_elif_5.py
+++ exercise/61_exe_if_elif_5.py
@@ -1,2 +1,13 @@
 """write a program to findout day of week(monday/tuesday/wednesday) from given date.  
 https://www.wikihow.com/Calculate-the-Day-of-the-Week"""
+day=int(input("Enter day of month :"))
+mon=int(input("Enter number of month :"))
+lm=26-0  #Year ending digit
+rf=lm//4 #Divinding by 4
+lm+=rf #Adding with reminder
+lm+=6 #adding 6
+lis=["",0,3,3,6,1,4,6,2,5,0,3,5] #value of table according to calender
+days=["Sunday","Monday","Tuesday","Wednesday","Thrsday","Friday","Sunday"] #days of week
+res=lm+lis[mon]+day #Geting value from lis
+res%=7  #Getting fay index
+print(days[res])
