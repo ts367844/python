6fcee19ca1c2d91803c11f49ae169bcf5b2c5d8e
ts367844 <tushar367844@gmail.com>
2026-01-24 08:40:40 +0530

    i else

--- 20_if_else_1.py
+++ 20_if_else_1.py
@@ -1,15 +1,15 @@
 # write a program to find out which is cheaper product to purchase from 2 product's weight and price. 
 
-price1 = float(input("Enter price of product 1 : "))
-weight1=float(input("Enter weight of product 1 :"))
+p1 = float(input("Enter price of product 1 : "))
+w1=float(input("Enter weight of product 1 :"))
 
-price2 = float(input("Enter price of product 2 : "))
-weight2=float(input("Enter weight of product 2 :"))
+p2 = float(input("Enter price of product 2 : "))
+w2=float(input("Enter weight of product 2 :"))
 
-pp1 = price1/weight1
-pp2 = price2/weight2
+price_per_gram1 = p1/w1
+price_per_gram2 = p2/w2
 
-if pp1>pp2:
-    print("Second product is cheaper : ",pp1-pp2)
+if price_per_gram1>price_per_gram2:
+    print("Second product is cheaper : ",price_per_gram1-price_per_gram2)
 else:
-    print("first is cheaper  : ",pp2-pp1)
+    print("first is cheaper  : ",price_per_gram2-price_per_gram1)
