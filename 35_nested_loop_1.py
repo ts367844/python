e9f35c4fc5c7d724662c58b3039053242ba62fa7
ts367844 <tushar367844@gmail.com>
2026-01-28 12:01:55 +0530

    nested loop

---
+++ 35_nested_loop_1.py
@@ -0,0 +1,11 @@
+'''
+*
+* *
+* * *
+* * * *
+* * * * *
+'''
+for row in range(1,6):
+    for column in range(1,row+1):
+        print("*",end=' ')
+    print("")
---
+++ 36_nested_loop_2.py
@@ -0,0 +1,11 @@
+'''
+* * * * *
+* * * *
+* * *
+* *
+*
+'''
+for row in range(6,1,-1):
+    for column in range(1,row):
+        print("*",end=' ')
+    print("")
