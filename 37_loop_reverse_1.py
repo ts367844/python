2241478497e8000a8aa6852c3a2e1240f7a8afa0
ts367844 <tushar367844@gmail.com>
2026-02-08 15:18:01 +0530

    reverse loop

---
+++ 37_loop_reverse_1.py
@@ -0,0 +1,7 @@
+# write a program to display list in reverse 
+cars = ["Maruti Suzuki","Hyundai","Tata Motors","Mahindra","Toyota","Honda","Kia","Skoda","Volkswagen","Renault"]
+print(cars)
+
+
+for car in reversed(cars):
+    print(car)
