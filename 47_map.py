8c6108c2b2daf0f6fdbcc02d2f51e92774293c96
ts367844 <tushar367844@gmail.com>
2026-02-18 20:23:16 +0530

    map

---
+++ 47_map.py
@@ -0,0 +1,5 @@
+import math
+#convert negative into postive using map and lambda 
+numbers = [-45, 12, 78, -3, 0, 56, -89, 34, -21, 67, 5, -14, 92, -76, 18, -9, 41, -60, 23, -2]
+positive_only = list(map(lambda num: math.fabs(num),numbers))
+print(positive_only)
