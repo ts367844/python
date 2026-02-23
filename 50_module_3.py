151cb1d149837294835168978ba1635945ed1f3b
ts367844 <tushar367844@gmail.com>
2026-02-23 17:16:19 +0530

    module

---
+++ 50_module_3.py
@@ -0,0 +1,10 @@
+import math 
+number = -3.39
+print(f"ceil value of {number} ",math.ceil(number))
+print(f"floor value of {number} ",math.floor(number))
+print(f"truncated value of {number} ",math.trunc(number))
+print("absolute value -5 ",math.fabs(-5))
+print("factorial of 5 ",math.factorial(5))
+print(f"copysign of {number} ",math.copysign(number,1))
+print(f"output of modf ",math.modf(number))
+print(f"reminder of 10 divided by 3",math.fmod(10,3))
