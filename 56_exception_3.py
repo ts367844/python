732f021fcb1cffbeca28812c0646fea4c277f20c
ts367844 <tushar367844@gmail.com>
2026-03-01 20:52:48 +0530

    exception

--- 56_exception_3.py
+++ 56_exception_3.py
@@ -4,6 +4,7 @@ total = 0
 scoreboard = [83, 12, 57, 4, -96, 28, 71, 39, 65, 2, 90]
 # scoreboard = [None,12, 'Rinku',57, 4, 96, 28, 71, 39, 65, 2, 90]
 # scoreboard = ['Rinku','Rohit','Kohli']
+
 # scoreboard = None 
 count = 0
 for runs in scoreboard:
